f70c3ea3127e9dc694a6321d5ff99b3c916e5cbf
Alican Kurus <alicankurus.dev@gmail.com>
2023-08-12 10:28:06 +0300

    done

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -5,7 +5,22 @@ const Content = () => {
   return (
     <div>
       <h2>React js</h2>
-      <p>React is a way to go further</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias perspiciatis odit vero asperiores, beatae eveniet tenetur, in cumque obcaecati quidem itaque rerum, velit reprehenderit tempora deserunt ad a error. Porro.
+        
+        name
+        sur
+        jus sit
+        jus sit exp 
+        eco sit
+        <sin />
+        <fam></fam>
+        cont 
+        short brf
+        ref con 
+        red date
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias perspiciatis odit vero asperiores, beatae eveniet tenetur, in cumque obcaecati quidem itaque rerum, velit reprehenderit tempora deserunt ad a error. Porro.
+        </p>
       <img src={logo} style={{width:"250px"}} alt="" />
     </div>
   )
