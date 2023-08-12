05bf0a9c738b6370f2f0c2edf1b96ce7790c631a
Alican Kurus <alicankurus.dev@gmail.com>
2023-08-12 10:11:29 +0300

    content created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Content from "./components/content/Content";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Content/>
+      <Footer/>
     </div>
   );
 };
--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from "../../logo.svg"
 
 const Content = () => {
   return (
-    <div>Content</div>
+    <div>
+      <h2>React js</h2>
+      <p>React is a way to go further</p>
+      <img src={logo} style={{width:"250px"}} alt="" />
+    </div>
   )
 }
 
